fe18bbb9cbace8d1c77e1b4d5fb1b942cdacfe2b
Kumar7708 <kumarbevara7708@gmail.com>
2021-02-22 21:54:44 +0530

    firat commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    socket.broadcast.emit('message', msg);
+  });
+
+});
+
+
+http.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
